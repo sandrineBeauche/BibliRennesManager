1bfb0a15f6ff2cd296a73717f422cae6f8717df0
sandrine <sandrine.beauche@gmail.com>
2021-05-28 14:40:06 +0200

    parse login form

--- src/bibliRennesManager/services/bibliRennesScraper/bibliRennesScraper/spiders/bibliRennesSpider.py
+++ src/bibliRennesManager/services/bibliRennesScraper/bibliRennesScraper/spiders/bibliRennesSpider.py
@@ -1,6 +1,7 @@
 from scrapy.http import FormRequest
 from scrapy.crawler import CrawlerProcess
 import scrapy
+from scrapy_headless import SeleniumRequest
 
 # class ServerError(Error):
 #
@@ -12,30 +13,38 @@ import scrapy
 class BibliRennesSpider(scrapy.Spider):
     name = "bibliRennes"
 
-    login_url = "https://sbib.si.leschampslibres.fr/iii/cas/login?service=https%3A%2F%2Fopac.si.leschampslibres.fr%3A443%2Fiii%2Fencore%2Fj_acegi_cas_security_check&lang=frf&suite=pearl"
-
+    login_url = "https://sbib.si.leschampslibres.fr/iii/cas/login"
     account_url = "https://opac.si.leschampslibres.fr/iii/encore/myaccount?lang=frf"
+    logout_url = "https://sbib.si.leschampslibres.fr/iii/cas/logout"
 
     def start_requests(self):
-        yield FormRequest(self.login_url,
-                          formdata={"code": "23500002705434",
-                                    "pin": "9ewxxjIUAfLcYGIKY1CT"},
-                          callback=self.parse_login_response)
+        yield SeleniumRequest(url=self.login_url,
+                              callback=self.parse_login)
+
+    def parse_login(self, response):
+        driver = response.interact
+        eltCode = driver.find_element_by_css_selector("input#code")
+        eltCode.send_keys("23500002705434")
+        eltPin = driver.find_element_by_css_selector("input#pin")
+        eltPin.send_keys("9ewxxjIUAfLcYGIKY1CT")
+        response = response.click('//input[@name="submit"]')
+
+        if response.css("div.accountSummary") != []:
+            print("coucou")
+
+        self.parse_account(response)
+
+        yield Request(url=self.logout_url)
 
 
-    def parse_login_response(self, response):
-        if response.status == 200:
-            yield scrapy.Request(self.account_url, callback=self.parse_account, dont_filter=True)
-        else:
-            raise NameError("Server connection exeption")
 
     def parse_account(self, response):
         if response.status != 200:
             raise NameError("Server connection exeption")
 
 
-    def parse_account_response(self, response):
-        pass
+
+
 
     def parse_details_response(self, response):
         pass
