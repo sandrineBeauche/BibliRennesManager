f056dcfd4ae50d33ba5ba940251e76e63f7591be
sandrine <sandrine.beauche@gmail.com>
2021-06-05 00:02:27 +0200

    resolve bug when getting scraper settings

--- src/bibliRennesManager/services/bibliRennesScraper/bibliRennesScraperLauncher.py
+++ src/bibliRennesManager/services/bibliRennesScraper/bibliRennesScraperLauncher.py
@@ -1,10 +1,18 @@
+import os
+
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
+from os import getcwd, chdir, path
 
 
 def launch():
-    process = CrawlerProcess(get_project_settings())
+    pwd = getcwd()
+    scraper_dir = path.dirname(path.realpath(__file__))
+    chdir(scraper_dir)
+    settings = get_project_settings()
+    process = CrawlerProcess(settings)
     process.crawl("bibliRennes")
+    chdir(pwd)
     process.start()
 
 
