a0ac0498ce23c734442970457f3762e8ce4e5569
sandrine <sandrine.beauche@gmail.com>
2021-06-14 22:21:57 +0200

    login to bibli rennes

--- src/bibliRennesManager/services/bibliRennesScraper/bibliRennesScraper/spiders/bibliRennesSpider.py
+++ src/bibliRennesManager/services/bibliRennesScraper/bibliRennesScraper/spiders/bibliRennesSpider.py
@@ -1,13 +1,7 @@
-from scrapy.http import Request
-from bibliRennesManager.services.bibliRennesScraper.bibliRennesScraper.http import SessionRequest
-from scrapy.crawler import CrawlerProcess
+from src.bibliRennesManager.services.bibliRennesScraper.bibliRennesScraper.http import SessionRequest
 import scrapy
+from scrapy_playwright.page import PageCoroutine
 
-# class ServerError(Error):
-#
-#     def __init__(self, serverUrl, message):
-#         self.serverUrl = serverUrl
-#         self.message = message
 
 
 class BibliRennesSpider(scrapy.Spider):
@@ -20,37 +14,38 @@ class BibliRennesSpider(scrapy.Spider):
     cardId = None
     cardPassword = None
 
-
     def __init__(self, cardId=None, cardPassword=None, *args, **kwargs):
         super(BibliRennesSpider, self).__init__(*args, **kwargs)
         self.cardId = cardId
         self.cardPassword = cardPassword
 
     def start_requests(self):
-        req = SessionRequest(url=self.login_url, 
-                        callback=self.parse_login_response,
-                        sessionId=self.cardId)
+        page_coro = [
+            PageCoroutine("fill", selector="input#code", value=self.cardId),
+            PageCoroutine("fill", selector="input#pin", value=self.cardPassword),
+            PageCoroutine("click", selector='//input[@name="submit"]'),
+            PageCoroutine("wait_for_load_state")
+        ]
+        req = SessionRequest(url=self.login_url,
+                             callback=self.parse_login_response,
+                             sessionId=self.cardId,
+                             page_coro=page_coro)
         yield req
 
-
     def parse_login_response(self, response):
-        page = response.meta["playwright_page"]
-        page.fill("input#code", self.cardId)
-        page.fill("input#pin", self.cardPassword)
-        page.click('//input[@name="submit"]')
-        
-        yield scrapy.Request(self.account_url, callback=self.parse_account, dont_filter=True,)
-        
+        divs = response.css(".success")
+        if len(divs) == 1:
+            self.logger.info("Successfully logged to BibliRennes with card " + self.cardId)
+            yield SessionRequest(url=self.account_url,
+                                 callback=self.parse_account,
+                                 sessionId=self.cardId)
+
     def parse_account(self, response):
         if response.status != 200:
             raise NameError("Server connection exeption")
-
 
     def parse_account_response(self, response):
         pass
 
     def parse_details_response(self, response):
         pass
-
-
-
