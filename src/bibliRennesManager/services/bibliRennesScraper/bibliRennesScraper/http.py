214c1e142149aabf5de23d1204eaddee709815d7
sandrine <sandrine.beauche@gmail.com>
2021-06-14 22:21:33 +0200

    add page coroutine to session request to wait for them

--- src/bibliRennesManager/services/bibliRennesScraper/bibliRennesScraper/http.py
+++ src/bibliRennesManager/services/bibliRennesScraper/bibliRennesScraper/http.py
@@ -1,9 +1,12 @@
 from scrapy.http.request import Request
 
 class SessionRequest(Request):
-    def __init__(self, sessionId=None, *args, **kwargs):
+    def __init__(self, sessionId=None, page_coro=None, *args, **kwargs):
         self.sessionId = sessionId
+        meta_dict = {"playwright": True, "playwright_include_page": True}
+        if page_coro is not None:
+            meta_dict["playwright_page_coroutines"] = page_coro
         super().__init__(
             dont_filter=True, 
-            meta={"playwright": True, "playwright_include_page": True}, 
+            meta=meta_dict,
             *args, **kwargs)
