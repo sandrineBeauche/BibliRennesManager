b7eb03aae2b1cb3c60c651364d223a42cdb5e8de
sandrine <sandrine.beauche@gmail.com>
2021-06-26 22:35:29 +0200

    refactor test to use pytest fixtures

--- test/bibliRennesScraper/test_items.py
+++ test/bibliRennesScraper/test_items.py
@@ -1,31 +1,43 @@
 from src.bibliRennesManager.services.bibliRennesScraper.bibliRennesScraper.items import BookItem, BookLoader
-
-def perform_property(loader_cls, propertyName, values):
-    for current in values:
-        loader = loader_cls()
-        loader.add_value("title", current["value"])
-        item = loader.load_item()
-        assert item.title == current["expected"]
+import pytest
 
 
-def test_book_loader_title():
-    titles = [
-        {
-            "value": "L'imagerie français-arabe / conception et texte Emilie Beaumont",
-            "expected": "L'imagerie français-arabe"
-        },
-        {
-            "value": " 	Apprendre l'anglais en chantant [Texte imprimé] / orchestrations Lucas Sanner, Renan Napoli ; interprètes Sarah Tullamore et Yann Marshall ; illustrations et conception graphique Adeline Ruel",
-            "expected": "Apprendre l'anglais en chantant"
-        },
-        {
-            "value": " 	Bienvenue au club ! écrit par Gwenaëlle Boulet ; illustré par Vincent Caut",
-            "expected": "Bienvenue au club !"
-        },
-        {
-            "value": "A la folie ! [Texte imprimé] / Zep",
-            "expected": "A la folie !"
-                        ""
-        }
-    ]
-    perform_property(lambda: BookLoader(item=BookItem()), "title", titles)
+@pytest.fixture
+def loader():
+    return BookLoader(item=BookItem())
+
+
+@pytest.fixture
+def item(loader):
+    def get_item(property_name, value):
+        loader.add_value(property_name, value)
+        return loader.load_item()
+    return get_item
+
+
+@pytest.fixture
+def item_title(item):
+    def perform(value, expected):
+        it = item("title", value)
+        assert it.title == expected
+    return perform
+
+
+def test_book_loader_title1(item_title):
+    item_title("L'imagerie français-arabe / conception et texte Emilie Beaumont",
+               "L'imagerie français-arabe")
+
+
+def test_book_loader_title2(item_title):
+    item_title(" 	Apprendre l'anglais en chantant [Texte imprimé] / orchestrations Lucas Sanner, Renan Napoli ; interprètes Sarah Tullamore et Yann Marshall ; illustrations et conception graphique Adeline Ruel",
+               "Apprendre l'anglais en chantant")
+
+
+def test_book_loader_title3(item_title):
+    item_title(" 	Bienvenue au club ! écrit par Gwenaëlle Boulet ; illustré par Vincent Caut",
+               "Bienvenue au club !")
+
+
+def test_book_loader_title4(item_title):
+    item_title("A la folie ! [Texte imprimé] / Zep",
+               "A la folie !")
