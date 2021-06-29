738390e89ea82db87222796fa1fcaa2da731d338
Sandrine Ben Mabrouk <sandrine.beauche@gmail.com>
2021-06-29 22:14:26 +0200

    improve item testing

--- src/bibliRennesManager/services/bibliRennesScraper/bibliRennesScraper/items.py
+++ src/bibliRennesManager/services/bibliRennesScraper/bibliRennesScraper/items.py
@@ -7,8 +7,9 @@ from datetime import datetime
 import scrapy
 from dataclasses import dataclass, field
 from typing import Optional
-from itemloaders.processors import TakeFirst, MapCompose, Join, Compose
+from itemloaders.processors import Identity, TakeFirst, MapCompose, Join, Compose
 from scrapy.loader import ItemLoader
+from scrapy.utils.deprecate import create_deprecated_class
 
 
 @dataclass
@@ -18,6 +19,7 @@ class BookItem:
     title: Optional[str] = field(default=None)
     authors: Optional[str] = field(default=None)
     cote: Optional[str] = field(default=None)
+    status: Optional[str] = field(default=None)
     library: Optional[str] = field(default=None)
     deadline: Optional[datetime] = field(default=None)
     reservation: Optional[bool] = field(default=False)
@@ -37,8 +39,40 @@ def extract_title(title: str):
         return title
 
 
+def extract_author(author: str):
+    if "(" in author:
+        return author.split("(")[0]
+    else:
+        return author
+
+def stripAll(value: str): 
+    value.strip("\n\t ")
+
+
+def extract_deadline(value: str):
+    return datetime.strptime(value, "RETOUR %d-%m-%y").date()
+
+
+class BooleanOr:
+    def __call__(self, values):
+        return any(values)
+
+BooleanOrProc = create_deprecated_class("BooleanOr", BooleanOr)
+
+
 class BookLoader(ItemLoader):
     default_input_processor = MapCompose(str.strip)
     default_output_processor = TakeFirst()
 
     title_in = MapCompose(extract_title, str.strip)
+    
+    authors_in = MapCompose(extract_author, str.strip)
+    authors_out = Join(separator=',')
+
+    status_in = MapCompose(stripAll)
+
+    deadline_in = MapCompose(str.strip, extract_deadline)
+    renewed_in = Identity()
+    renewed_out = BooleanOrProc()
+    reservation_in = Identity()
+    reservation_out = BooleanOrProc()
--- test/bibliRennesScraper/test_items.py
+++ test/bibliRennesScraper/test_items.py
@@ -1,6 +1,10 @@
+import hamcrest
+from hamcrest.library.object.hasproperty import has_property
 from src.bibliRennesManager.services.bibliRennesScraper.bibliRennesScraper.items import BookItem, BookLoader
 import pytest
-
+from datetime import date
+from hamcrest.core import assert_that
+from hamcrest.core.core.isequal import equal_to
 
 @pytest.fixture
 def loader():
@@ -9,35 +13,41 @@ def loader():
 
 @pytest.fixture
 def item(loader):
-    def get_item(property_name, value):
+    def perform(property_name, value, expected):
         loader.add_value(property_name, value)
-        return loader.load_item()
-    return get_item
-
-
-@pytest.fixture
-def item_title(item):
-    def perform(value, expected):
-        it = item("title", value)
-        assert it.title == expected
+        item = loader.load_item()
+        assert_that(item, has_property(property_name, equal_to(expected)))
     return perform
 
 
-def test_book_loader_title1(item_title):
-    item_title("L'imagerie français-arabe / conception et texte Emilie Beaumont",
-               "L'imagerie français-arabe")
+
+def test_book_loader_title1(item):
+    item("title", 
+         "L'imagerie français-arabe / conception et texte Emilie Beaumont",
+         "L'imagerie français-arabe")
 
 
-def test_book_loader_title2(item_title):
-    item_title(" 	Apprendre l'anglais en chantant [Texte imprimé] / orchestrations Lucas Sanner, Renan Napoli ; interprètes Sarah Tullamore et Yann Marshall ; illustrations et conception graphique Adeline Ruel",
-               "Apprendre l'anglais en chantant")
+def test_book_loader_title2(item):
+    item("title",
+         " 	Apprendre l'anglais en chantant [Texte imprimé] / orchestrations Lucas Sanner, Renan Napoli ; interprètes Sarah Tullamore et Yann Marshall ; illustrations et conception graphique Adeline Ruel",
+         "Apprendre l'anglais en chantant")
 
 
-def test_book_loader_title3(item_title):
-    item_title(" 	Bienvenue au club ! écrit par Gwenaëlle Boulet ; illustré par Vincent Caut",
-               "Bienvenue au club !")
+def test_book_loader_title3(item):
+    item("title",
+         " 	Bienvenue au club ! écrit par Gwenaëlle Boulet ; illustré par Vincent Caut",
+         "Bienvenue au club !")
 
 
-def test_book_loader_title4(item_title):
-    item_title("A la folie ! [Texte imprimé] / Zep",
-               "A la folie !")
+def test_book_loader_title4(item):
+    item("title",
+         "A la folie ! [Texte imprimé] / Zep",
+         "A la folie !")
+
+
+def test_book_loader_deadline(item):
+    item("deadline", "\n   RETOUR 06-07-21         \n", date(2021,7,6))
+
+
+def test_book_loader_renewed(item):
+    item("renewed", True, True)
